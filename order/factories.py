307d5237940f2ba53871eb3398c477a139c0ebfa
Antonio Pereira <anthonyesany0712@gmail.com>
2023-09-17 14:47:51 -0300

    update bookstore

---
+++ order/factories.py
@@ -0,0 +1,4 @@
+#1 declarou os modelos no admin.py de cada aplicativo
+#2 dentro __init__.py nós declaramos os modelos
+#3 declaramos os nossos apps (product e order) dentro do bookstore project (settings.py)
+#4 deletar o models.py porque nós já criamos um diretório
