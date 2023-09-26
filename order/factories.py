b3c1307b6bd6d40d680f91e4d1a20361d5dc5448
Antonio Pereira <anthonyesany0712@gmail.com>
2023-09-26 18:00:45 -0300

    update bookstore

--- order/factories.py
+++ order/factories.py
@@ -2,3 +2,32 @@
 #2 dentro __init__.py nós declaramos os modelos
 #3 declaramos os nossos apps (product e order) dentro do bookstore project (settings.py)
 #4 deletar o models.py porque nós já criamos um diretório
+
+import factory
+from django.contrib.auth.models import User
+
+from order.models import Order
+from product.factories import ProductFactory
+
+class UserFactory(factory.DjangoModelFactory):
+  email = factory.Faker('pystr')
+  username = factory.Faker('pystr')
+
+  class Meta:
+    model = User
+
+class OrderFactory(factory.DjangoModelFactory):
+  user = factory.SubFactory(UserFactory)
+
+  @factory.post_generation
+  def product(self, create, extracted, **kwargs):
+    if not create:
+      return
+
+    if extracted:
+      for product in extracted:
+        self.product.add(product)
+
+  class Meta:
+    model = Order
+
