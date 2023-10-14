4216cdbb751cd9017daf5327e42801b35134c61d
Antonio Pereira <anthonyesany0712@gmail.com>
2023-10-14 16:01:32 -0300

    update urls and factories

--- order/urls.py
+++ order/urls.py
@@ -3,7 +3,8 @@ from rest_framework import routers
 
 from order import viewsets
 
-router = register(r'order', viewsets.OrderViewSet, basename='order')
+router = routers.SimpleRouter()
+router.register(r'order', viewsets.OrderViewSet, basename='order')
 
 urlpatterns = [
   path('', include(router.urls)),
