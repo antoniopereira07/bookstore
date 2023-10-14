73432607c485efb955731dbea0eb0b2aa418a341
Antonio Pereira <anthonyesany0712@gmail.com>
2023-10-14 16:03:13 -0300

    update serializers

--- product/serializers/category_serializer.py
+++ product/serializers/category_serializer.py
@@ -10,4 +10,4 @@ class CategorySerializer(serializers.ModelSerializer):
       'description',
       'active',
     ]
-    extra_kwargs = {"slug": {"required": False}}
+    extra_kwargs = {'slug': {'required': False}}
