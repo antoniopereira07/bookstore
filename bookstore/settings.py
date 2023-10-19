b07f03ff5b126121d03c8762b47de3320b536bf3
Antonio Pereira <anthonyesany0712@gmail.com>
2023-10-19 18:59:11 -0300

    modifying settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,8 +42,8 @@ INSTALLED_APPS = [
     "order",
     "product",
     "rest_framework",
-    # "debug_toolbar",
     "rest_framework.authtoken",
+    # "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
