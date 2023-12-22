34a069bcf8d87fcf5d410491665195099f22f519
Antonio Pereira <anthonyesany0712@gmail.com>
2023-12-22 20:11:11 -0300

    update the make check

--- product/migrations/0001_initial.py
+++ product/migrations/0001_initial.py
@@ -49,7 +49,8 @@ class Migration(migrations.Migration):
                 ),
                 ("price", models.PositiveIntegerField(null=True)),
                 ("active", models.BooleanField(default=True)),
-                ("category", models.ManyToManyField(blank=True, to="product.category")),
+                ("category",
+                 models.ManyToManyField(blank=True, to="product.category")),
             ],
         ),
     ]
