8748a3e933c2c2cf0c0d6945f0d1a5c16c623487
Antonio Pereira <anthonyesany0712@gmail.com>
2023-12-27 18:41:34 -0300

    update product_serializer

--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -6,7 +6,9 @@ from product.serializers.category_serializer import CategorySerializer
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True, many=True)
-    categories_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), write_only=True, many=True)
+    categories_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        write_only=True, many=True)
 
     class Meta:
         model = Product
