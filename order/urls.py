7b61388ec63b9bbe2a80be8a8ef7a8291c51f2a0
Antonio Pereira <anthonyesany0712@gmail.com>
2024-01-20 17:16:53 -0300

    file corrections

--- order/urls.py
+++ order/urls.py
@@ -3,7 +3,6 @@ from rest_framework import routers
 
 from order import viewsets
 
-
 router = routers.SimpleRouter()
 router.register(r"order", viewsets.OrderViewSet, basename="order")
 
--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,6 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-
-
 from rest_framework.authentication import (
     SessionAuthentication,
     BasicAuthentication,
@@ -19,6 +17,5 @@ class OrderViewSet(ModelViewSet):
         TokenAuthentication,
     ]
     permission_classes = [IsAuthenticated]
-
-    serializer_class = OrderSerializer
     queryset = Order.objects.all().order_by("id")
+    serializer_class = OrderSerializer
