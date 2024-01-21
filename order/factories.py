a9ee7b10317b4ac7bbfd33e7903bc9b7349313b9
Antonio Pereira <anthonyesany0712@gmail.com>
2024-01-20 23:51:38 -0300

    update order

--- order/factories.py
+++ order/factories.py
@@ -1,7 +1,7 @@
 import factory
 from django.contrib.auth.models import User
 from order.models import Order
-from product.factories import ProductFactory
+# from product.factories import ProductFactory
 
 
 class UserFactory(factory.django.DjangoModelFactory):
