a1604db1b0de03ca6b3ab2396bba06e5ddc98ae0
Antonio Pereira <anthonyesany0712@gmail.com>
2024-03-07 22:44:50 -0300

    feat: add build actions demo

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,10 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-
-from rest_framework.authentication import (
-    BasicAuthentication,
-    SessionAuthentication,
-    TokenAuthentication,
-)
 from rest_framework.permissions import IsAuthenticated
 
 from order.models.order import Order
@@ -12,11 +6,6 @@ from order.serializers.order_serializer import OrderSerializer
 
 
 class OrderViewSet(ModelViewSet):
-    authentication_classes = [
-        SessionAuthentication,
-        BasicAuthentication,
-        TokenAuthentication,
-    ]
     permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
     queryset = Order.objects.all().order_by("id")
